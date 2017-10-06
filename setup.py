49dfcee4331af1a8eae183a9c220ff833473de11
John Nolette <john@neetgroup.net>
2017-10-06 18:53:10 -0400

    Created setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ppc',
+    description='PPC is an opinionated framework for structuring selenium tests. This project depends on pyselenium-js.',
+    version='0.0.1',
+    url='https://github.com/neetjn/py-page-controller',
+    author='John Nolette',
+    author_email='john@neetgroup.net',
+    license='Apache2',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    packages=['ppc']
+)
