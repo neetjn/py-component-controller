a201a5e91c1077671a8a55d55d5812b8e4c7b8fc
John Nolette <john@neetgroup.net>
2017-10-06 21:44:24 -0400

    Updated docstrings, project nomenclature.

--- py-page-controller/controller.py
+++ py-page-controller/controller.py
@@ -1,7 +1,16 @@
+import time
+from pyseleniumjs import E2EJS
+
+
 class Controller(object):
 
   def __init__(self, webdriver, base_url, components, **env):
+    """
+    :Description: Controller for managing components.
+    :param webdriver: Webdriver for controller and components to refernece.
+    """
     self.webdriver = webdriver
+    self.js = E2EJS(webdriver)
     self.base_url = base_url
     if not isinstance(components, (tuple, list, dict)):
       raise TypeError('Components must be either a tuple, list, or dictionary')
@@ -12,4 +21,92 @@ class Controller(object):
     else:
       self.components = [component(webdriver, env) for component in components]
 
-  def wait(self, )
+  @staticmethod
+  def __patch_webdriver():
+
+
+  @property
+  def location(self):
+    """
+    :Description: Fetch the current url of controller's webdriver instance.
+    :return: basestring
+    """
+    return self.webdriver.current_url
+
+  @property
+  def title(self):
+    """
+    :Description: Fetch the title of the controller's webdriver instance.
+    """
+    return self.webdriver.title
+
+  def navigate(self, route):
+    """
+    :Description: Navigate to a
+    """
+    self.webdriver.get('{location}/{route}'.format(
+      location = self.base_url,
+      route=route
+    ))
+
+  def dump_browser_logs(self, name=None):
+        """
+        :Description: Dumps browser logs to local directory.
+        :Warning: `self.js.console_logger` must be executed to store logs.
+        :param name: Name log file dropped to disk, will default to timestamp if not specified.
+        :type name: basestring
+        """
+        try:
+            logs = self.js.console_dump()
+            timestamp = str(int(time.time()))
+            log_name = 'console.%s.log' % (('%s.%s' % (name, timestamp)) if name else timestamp)
+            if not os.path.exists('artifacts'):
+                os.makedirs('artifacts')
+            with open('artifacts/%s' % log_name, 'a') as logfile:
+                logfile.write(logs)
+        except WebDriverException:
+            self.logger.critical('Browser console was not overridden, could not return any logs.')
+
+    def window_by_handle(self, window=None):
+        """
+        :Description: Changes to specified window or most recently opened.
+        :param window: (window handle) If specified, browser instance will focus on window handle, else focus on parent window.
+        """
+        self.browser.switch_to_window(self.windows.parent) \
+          if window is None else self.browser.switch_to_window(window)
+
+    def window_by_title(self, title, graceful=False):
+        # todo: refactor these to use the window function already included in the controller (self.windows)
+        """
+        :Description: Changes to window context by window title.
+        :param title: Title of window to switch into.
+        :type title: basestring
+        :param graceful: Adds leniency to window title search.
+        :type graceful: bool
+        """
+        for handle in self.browser.window_handles:
+            self.browser.switch_to_window(handle)
+            if graceful and title in self.title:
+                return True
+            if not graceful and self.title == title:
+                return True
+        return False
+
+    def window_by_location(self, location, graceful=False):
+        # todo: refactor these to use the window function already included in the controller (self.windows)
+        """
+        :Description: Changes to window context by window path.
+        :param location: Path of window to switch into.
+        :type location: basestring
+        :param graceful: Adds leniency to window path search.
+        :type graceful: bool
+        """
+        for handle in self.browser.window_handles:
+            self.browser.switch_to_window(handle)
+            if graceful and location in self.location:
+                return True
+            if not graceful and self.location == location:
+                return True
+        return False
+
+  def wait(self, timeout, condition=None):
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ppc',
-    description='PPC is an opinionated framework for structuring selenium tests. This project depends on pyselenium-js.',
+    name='pcc',
+    description='PCC is an opinionated framework for structuring selenium tests. This project depends on pyselenium-js.',
     version='0.0.1',
-    url='https://github.com/neetjn/py-page-controller',
+    url='https://github.com/neetjn/py-component-controller',
     author='John Nolette',
     author_email='john@neetgroup.net',
     license='Apache2',
@@ -14,5 +14,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7'
     ],
-    packages=['ppc']
+    packages=['pcc']
 )
