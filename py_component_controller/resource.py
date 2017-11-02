a18a381aad2bc947baa4fbfb9d796b90a4599247
John Nolette <john@neetgroup.net>
2017-11-02 10:46:07 -0400

    Update resource.py

--- py_component_controller/resource.py
+++ py_component_controller/resource.py
@@ -4,5 +4,10 @@ class Resource(object):
         """
         :Description: Base object for shenanigans.
         """
+
+        required_fields = self.meta.get('required_fields', None)
+        if required_fields and not any(field not in fields for field in check.iterkeys()):
+            raise AttributeError('Required fields "{}" were not available'.format(required_fields))
+ 
         for prop, val in kwargs.iteritems():
             setattr(self, prop, val)
