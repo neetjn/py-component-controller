d789e50bacb2c32b026f446e8f008bfb7c33c58b
John Nolette <john@neetgroup.net>
2017-12-15 10:29:18 -0500

    Created component element test module

---
+++ tests/test_component_elements.py
@@ -0,0 +1,16 @@
+from tests.utils import BaseTest
+
+
+class TestElement(BaseTest):
+
+  def test_element_wrapper():
+    pass
+    
+  def test_element_wrapper_check():
+    pass
+    
+  def test_elements_wrapper():
+    pass
+    
+  def test_elements_wrapper_checks():
+    pass
