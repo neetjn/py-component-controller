eeeb1b15b846ef25d31234c38697920c854c289c
John Nolette <john@neetgroup.net>
2017-12-16 11:23:34 -0500

    Fixed test formatting

--- tests/test_component.py
+++ tests/test_component.py
@@ -3,8 +3,8 @@ from tests.utils import BaseTest
 
 class TestComponent(BaseTest):
 
-  def test_component_attributes():
-    pass
-    
-  def test_component_controller():
-    pass
+    def test_component_attributes(self):
+        pass
+
+    def test_component_controller(self):
+        pass
--- tests/test_component_elements.py
+++ tests/test_component_elements.py
@@ -3,14 +3,14 @@ from tests.utils import BaseTest
 
 class TestElement(BaseTest):
 
-  def test_element_wrapper():
-    pass
-    
-  def test_element_wrapper_check():
-    pass
-    
-  def test_elements_wrapper():
-    pass
-    
-  def test_elements_wrapper_checks():
-    pass
+    def test_element_wrapper(self):
+        pass
+
+    def test_element_wrapper_check(self):
+        pass
+
+    def test_elements_wrapper(self):
+        pass
+
+    def test_elements_wrapper_checks(self):
+        pass
