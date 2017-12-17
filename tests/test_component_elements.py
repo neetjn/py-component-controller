6947a0ca56bb1b0efd59d9f635230072d1342618
John Nolette <john@neetgroup.net>
2017-12-16 23:37:10 -0500

    Added tests for component element wait

--- tests/test_component_elements.py
+++ tests/test_component_elements.py
@@ -27,6 +27,16 @@ class TestElement(BaseTest):
         self.assertIsInstance(task.get(), WebElement)
         self.assertEqual(task.fmt(id=str(uuid4())).get(), None)
 
+    def test_element_wrapper_wait(self):
+        """test element wrapper wait"""
+        self.assertEqual(self.logo.wait_for(timeout=1), self.logo)
+        self.assertEqual(self.task.wait_for(timeout=1), None)
+
+    def test_element_wrapper_wait_visibility(self):
+        """test element wrapper visibility wait"""
+        self.assertEqual(self.logo.wait_visible(timeout=1), self.logo)
+        self.assertEqual(self.logo.wait_invisible(timeout=5), self.logo)
+
     def test_element_wrapper_check(self):
         """verify element wrapper check module"""
         self.assertTrue(self.logo.check.available())
