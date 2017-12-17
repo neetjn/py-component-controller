0bdea33eec9ffa630188e71d2a174d6681d38899
John Nolette <john@neetgroup.net>
2017-12-16 21:54:19 -0500

    Re-enabled controller tests

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -4,35 +4,35 @@ from tests.utils import BaseTest, HomePage
 
 class TestController(BaseTest):
 
-    # def test_controller_screenshot(self):
-    #     """test controller screenshot"""
-    #     file_name = self.app.screen_shot(prefix='test')
-    #     self.assertTrue(os.stat(file_name))
+    def test_controller_screenshot(self):
+        """test controller screenshot"""
+        file_name = self.app.screen_shot(prefix='test')
+        self.assertTrue(os.stat(file_name))
 
-    # def test_controller_wait(self):
-    #     """"test controller conditional wait"""
-    #     self.app.delete_tasks(tasks=1)
-    #     home = self.app.components.home
-    #     self.assertFalse(self.app.wait(
-    #         timeout=5, condition=lambda: home.tasks.count() == 3, reverse=True))
-    #     self.assertTrue(self.app.wait(
-    #         timeout=5, condition=lambda: home.tasks.count() == 1))
+    def test_controller_wait(self):
+        """"test controller conditional wait"""
+        self.app.delete_tasks(tasks=1)
+        home = self.app.components.home
+        self.assertFalse(self.app.wait(
+            timeout=5, condition=lambda: home.tasks.count() == 3, reverse=True))
+        self.assertTrue(self.app.wait(
+            timeout=5, condition=lambda: home.tasks.count() == 1))
 
-    # def test_controller_navigate(self):
-    #     """test controller navigation"""
-    #     self.app.navigate('!#/about')
-    #     self.assertEqual(self.app.location, self.app_url + '/!#/about')
+    def test_controller_navigate(self):
+        """test controller navigation"""
+        self.app.navigate('!#/about')
+        self.assertEqual(self.app.location, self.app_url + '/!#/about')
 
-    # def test_controller_env(self):
-    #     """test controller env resource is properly created"""
-    #     self.assertTrue(hasattr(self.app, 'env'))
-    #     self.assertEqual(self.app.env.created, self.created)
+    def test_controller_env(self):
+        """test controller env resource is properly created"""
+        self.assertTrue(hasattr(self.app, 'env'))
+        self.assertEqual(self.app.env.created, self.created)
 
-    # def test_controller_component_mapping(self):
-    #     """test controller components are instantiated as expected"""
-    #     self.assertTrue(hasattr(self.app, 'components'))
-    #     self.assertTrue(hasattr(self.app.components, 'home'))
-    #     self.assertIsInstance(self.app.components.home, HomePage)
+    def test_controller_component_mapping(self):
+        """test controller components are instantiated as expected"""
+        self.assertTrue(hasattr(self.app, 'components'))
+        self.assertTrue(hasattr(self.app.components, 'home'))
+        self.assertIsInstance(self.app.components.home, HomePage)
 
     def test_controller_window_detection(self):
         """test controller window detection"""
