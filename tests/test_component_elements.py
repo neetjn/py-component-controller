6d22ce83b14c093f94f5d38c92e3977902f24627
John Nolette <john@neetgroup.net>
2017-12-16 23:20:43 -0500

    Enabled component elements test, finished element wrapper check

--- tests/test_component_elements.py
+++ tests/test_component_elements.py
@@ -13,28 +13,36 @@ class TestElement(BaseTest):
         self.task = self.app.components.home.task
         self.tasks = self.app.components.home.tasks
 
-    # def test_element_wrapper(self):
-    #     self.assertIsInstance(self.logo, Element)
-    #     self.assertTrue(hasattr(self.logo, 'controller'))
-    #     self.assertTrue(hasattr(self.logo, 'selector'))
-    #     self.assertTrue(hasattr(self.logo, 'check'))
-    #     self.assertEqual(self.app, self.logo.controller)
+    def test_element_wrapper(self):
+        """test element wrapper instantiated as intended"""
+        self.assertIsInstance(self.logo, Element)
+        self.assertTrue(hasattr(self.logo, 'controller'))
+        self.assertTrue(hasattr(self.logo, 'selector'))
+        self.assertTrue(hasattr(self.logo, 'check'))
+        self.assertEqual(self.app, self.logo.controller)
 
-    # def test_element_wrapper_fmt(self):
-    #     task = self.task.fmt(id=1)
-    #     self.assertIsInstance(task.get(), WebElement)
-    #     self.assertEqual(task.fmt(id=str(uuid4())).get(), None)
+    def test_element_wrapper_fmt(self):
+        """test element wrapper selector formatting"""
+        task = self.task.fmt(id=1)
+        self.assertIsInstance(task.get(), WebElement)
+        self.assertEqual(task.fmt(id=str(uuid4())).get(), None)
 
     def test_element_wrapper_check(self):
-        self.assertTrue(self.task.check.visible())
-        self.assertTrue(self.app.wait(timeout=5, condition=self.task.check.invisible))
+        """verify element wrapper check module"""
+        self.assertTrue(self.logo.check.available())
+        self.assertTrue(self.logo.check.visible())
+        self.assertTrue(self.app.wait(timeout=5, condition=self.logo.check.invisible))
+        self.app.delete_tasks(tasks=2)
+        self.assertTrue(self.app.wait(timeout=5, condition=self.task.fmt(id=2).check.not_available))
 
-    # def test_elements_wrapper(self):
-    #     self.assertIsInstance(self.tasks, Elements)
-    #     self.assertTrue(hasattr(self.tasks, 'controller'))
-    #     self.assertTrue(hasattr(self.tasks, 'selector'))
-    #     self.assertTrue(hasattr(self.tasks, 'checks'))
-    #     self.assertEqual(self.app, self.tasks.controller)
+    def test_elements_wrapper(self):
+        """test element wrapper instantiated as intended"""
+        self.assertIsInstance(self.tasks, Elements)
+        self.assertTrue(hasattr(self.tasks, 'controller'))
+        self.assertTrue(hasattr(self.tasks, 'selector'))
+        self.assertTrue(hasattr(self.tasks, 'checks'))
+        self.assertEqual(self.app, self.tasks.controller)
 
-    # def test_elements_wrapper_checks(self):
-    #     pass
+    def test_elements_wrapper_checks(self):
+        """verify element wrapper checks module"""
+        pass
