0a621c7213ab298fc31b5f12a62df8bbf36b002c
John Nolette <john@neetgroup.net>
2017-12-18 12:09:40 -0500

    Added new test for is location

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -23,6 +23,21 @@ class TestController(BaseTest):
         self.app.navigate('!#/about')
         self.assertEqual(self.app.location, self.app_url + '/!#/about')
 
+    def test_controller_is_location(self):
+        """test controller is_location"""
+        self.app.navigate('!#/about')
+        self.assertTrue(self.app.is_location(self.app.location, self.app_url + '/!#/about'))
+        self.assertFalse(self.app.is_location('/!#/about'))
+        self.assertTrue(self.app.is_location('/!#/about', graceful=True))
+        self.assertTrue(
+            self.app.is_location(self.app.location, self.app_url + '/!#/about', timeout=1))
+        self.assertTrue(self.app.is_location('/!#/about', timeout=1, graceful=True))
+        self.assertFalse(self.app.is_location('/!#/about', timeout=1))
+        with self.assertRaises(RuntimeError):
+            self.app.is_location('/!#/about', error=True)
+        with self.assertRaises(RuntimeError):
+            self.app.is_location('/!#/about', timeout=1, error=True)
+
     def test_controller_env(self):
         """test controller env resource is properly created"""
         self.assertTrue(hasattr(self.app, 'env'))
