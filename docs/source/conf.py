287dc00c2dbfb43fb38310f544d8e833ff3d4eb7
John Nolette <john@neetgroup.net>
2017-12-23 21:22:10 -0500

    Removed external links opt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,6 @@ html_theme_options = {
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
