abdc1e80274f2fc9c8df07e3c05db4cdf8473c24
John Nolette <john@neetgroup.net>
2018-04-07 13:16:54 -0400

    Began adding test for component description

--- tests/test_component.py
+++ tests/test_component.py
@@ -6,20 +6,25 @@ from selenium import webdriver
 
 class TestComponent(BaseTest):
 
-    def test_component_controller(self):
-        """test component has reference to controller"""
-        self.assertTrue(hasattr(self.app.components.home, 'controller'))
-        self.assertIsInstance(self.app.components.home.controller, Controller)
-        self.assertEqual(self.app, self.app.components.home.controller)
+    # def test_component_controller(self):
+    #     """test component has reference to controller"""
+    #     self.assertTrue(hasattr(self.app.components.home, 'controller'))
+    #     self.assertIsInstance(self.app.components.home.controller, Controller)
+    #     self.assertEqual(self.app, self.app.components.home.controller)
 
-    def test_component_webdriver(self):
-        """test component has reference to webdriver"""
-        self.assertTrue(hasattr(self.app.components.home, 'browser'))
-        self.assertIsInstance(self.app.components.home.browser, webdriver.Chrome)
-        self.assertEqual(self.app.components.home.browser, self.app.browser)
+    # def test_component_webdriver(self):
+    #     """test component has reference to webdriver"""
+    #     self.assertTrue(hasattr(self.app.components.home, 'browser'))
+    #     self.assertIsInstance(self.app.components.home.browser, webdriver.Chrome)
+    #     self.assertEqual(self.app.components.home.browser, self.app.browser)
 
-    def test_component_env(self):
-        """test component has reference to env"""
-        self.assertTrue(hasattr(self.app.components.home, 'env'))
-        self.assertEqual(self.app.components.home.env, self.app.env)
-        self.assertEqual(self.app.components.home.env.created, self.created)
+    # def test_component_env(self):
+    #     """test component has reference to env"""
+    #     self.assertTrue(hasattr(self.app.components.home, 'env'))
+    #     self.assertEqual(self.app.components.home.env, self.app.env)
+    #     self.assertEqual(self.app.components.home.env.created, self.created)
+
+    def test_component_description(self):
+        """test component description works as intended"""
+        print self.app.components.home.__describe__
+        0/0
